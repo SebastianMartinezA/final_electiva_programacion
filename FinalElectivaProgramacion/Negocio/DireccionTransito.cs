3ab55b17a2347077943994b993678d0add320605
Matias Garcia <matu.garcia@hotmail.com.ar>
2022-06-21 21:03:19 -0300

    Add initial classes

---
+++ FinalElectivaProgramacion/Negocio/DireccionTransito.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class DireccionTransito
+    {
+        private List<Infraccion> infracciones;
+        private List<Suceso> sucesos;
+        private List<Pago> pagos;
+        private List<Vehiculo> vehiculos;
+
+        public DireccionTransito()
+        {
+        }
+    }
+}
---
+++ FinalElectivaProgramacion/Negocio/InfraccionGrave.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class InfraccionGrave : Infraccion
+    {
+        private static int Descuento25Dias;
+
+        public InfraccionGrave(int id, string descripcion, double importe) : base(id, descripcion, importe)
+        {
+
+        }
+
+        private static int GetDescuento25Dias()
+        {
+            return Descuento25Dias;
+        }
+
+        public static void SetDescuento25Dias(int desc)
+        {
+            Descuento25Dias = desc;
+        }
+
+        public override bool isGrave()
+        {
+            return true;
+        }
+    }
+}
